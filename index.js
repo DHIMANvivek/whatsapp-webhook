b6bc27996d8b2b54e07459fb72e18c11b7b0f9d5
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2024-02-08 15:46:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,16 @@ app.listen(process.env.PORT,()=>{
 });
 
 app.get("/webhook",(req,res)=>{
+    console.log("inside webhook")
    let mode=req.query["hub.mode"];
    let challange=req.query["hub.challenge"];
    let token=req.query["hub.verify_token"];
 
 
     if(mode && token){
-    console.log("inside mode");
+        console.log("inside mode and token");
         if(mode==="subscribe" && token===mytoken){
-            console.log("inside succes");
+            console.log("inside subscribe and token");
             res.status(200).send(challange);
         }else{
             res.status(403);
