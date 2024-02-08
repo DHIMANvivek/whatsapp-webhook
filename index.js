8aa2139a240ab6fda7b4dbc13c5101024d5f877b
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2024-02-08 16:50:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,21 +36,18 @@ app.get("/webhook", (req, res) => {
 
     console.log(mytoken , "mytoken is")
   
-    // Parse the query params
       let mode = req.query["hub.mode"];
       let token = req.query["hub.verify_token"];
       let challenge = req.query["hub.challenge"];
+
+    console.log(mode, token , challenge);
     
-      // Check if a token and mode is in the query string of the request
       if (mode && token) {
         console.log("inside mode and token")
-        // Check the mode and token sent is correct
         if (mode === "subscribe" && token === mytoken) {
-          // Respond with the challenge token from the request
           console.log("WEBHOOK_VERIFIED");
           res.status(200).send(challenge);
         } else {
-          // Respond with '403 Forbidden' if verify tokens do not match
           res.sendStatus(403);
         }
       }
