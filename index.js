ed8810315aa96c25bd254ffc797a989398c7a598
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2024-02-09 17:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/webhook", (req, res) => {
     }
   });
 
-app.post("/webhook",(req,res)=>{
+app.post("/webhook",(req,res)=>{ 
 
     let body_param=req.body;
 
@@ -47,99 +47,18 @@ app.post("/webhook",(req,res)=>{
 
                console.log("phone number "+phon_no_id);
                console.log("from "+from);
-            //    console.log("boady param "+msg_body , typeof msg_body);
+               console.log("boady param "+msg_body);
 
-               const whatsapp_message = {
-                messaging_product: "whatsapp",
-                to: from,
-                text: {
-                  body: "your message is " + msg_body
-                }
-              }
-
-              const whatsapp_template = {
-                "messaging_product": "whatsapp",
-                "to": "916283415102",
-                "type": "template",
-                "template": {
-                    "name": "vivek_test",
-                    "language": {
-                        "code": "en"
-                    }
-                }
-            }
-
-            const image = 
-            {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-            "to": "916283415102",
-            "type": "image",
-            "image": {
-                "link": "https://i.imgur.com/OevvBMO.jpeg"
-            }
-            }
-
-            const template_customn  = {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": "916283415102",
-                "type": "template",
-                "template": {
-                    "name": "vivek_2",
-                    "language": {
-                        "code": "en"
-                    }
-                },
-                "components": [
-                    {
-                        "type": "header",
-                        "parameters": [
-                           { "type": "image", 
-                                "link": "https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-                           }
-            
-                        ]
-                    },
-                    {
-                        "type": "body",
-                        "parameters": [
-                            {
-                                "type": "text",
-                                "text": "Ready To Chat"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "button",
-                        "sub_type": "quick_reply",
-                        "index": "0",
-                        "parameters": [
-                            {
-                                "type": "payload",
-                                "payload": "PAYLOAD"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "button",
-                        "sub_type": "quick_reply",
-                        "index": "1",
-                        "parameters": [
-                            {
-                                "type": "payload",
-                                "payload": "PAYLOAD"
-                            }
-                        ]
-                    }
-                ]
-            }
-
-            axios({
+               axios({
                 method: "POST",
                 url: "https://graph.facebook.com/v18.0/" + phon_no_id + "/messages?access_token=" + token,
-                data: whatsapp_message ? whatsapp_message : whatsapp_message
-                ,
+                data: {
+                  messaging_product: "whatsapp",
+                  to: from,
+                  text: {
+                    body: "your message is " + msg_body
+                  }
+                },
                 headers: {
                   "Content-Type": "application/json"
                 }
@@ -154,72 +73,7 @@ app.post("/webhook",(req,res)=>{
             }else{
                 res.sendStatus(404);
             }
-
-            // function isBusinessId(business_id) {
-            //     if (business_id) {
-            //     axios({
-            //         method: "POST",
-            //         url: "https://graph.facebook.com/v19.0/" + business_id + "/message_templates?access_token=" + token,
-            //         data: {
-            //             "name": "seasonal_promotion",
-            //             "language": "en",
-            //             "category": "MARKETING",
-            //             "components": [
-            //               {
-            //                 "type": "HEADER",
-            //                 "format": "TEXT",
-            //                 "text": "Our {{1}} is on!",
-            //                 "example": {
-            //                   "header_text": [
-            //                     "Summer Sale"
-            //                   ]
-            //                 }
-            //               },
-            //               {
-            //                 "type": "BODY",
-            //                 "text": "Shop now through {{1}} and use code {{2}} to get {{3}} off of all merchandise.",
-            //                 "example": {
-            //                   "body_text": [
-            //                     [
-            //                       "the end of August","25OFF","25%"
-            //                     ]
-            //                   ]
-            //                 }
-            //               },
-            //               {
-            //                 "type": "FOOTER",
-            //                 "text": "Use the buttons below to manage your marketing subscriptions"
-            //               },
-            //               {
-            //                 "type":"BUTTONS",
-            //                 "buttons": [
-            //                   {
-            //                     "type": "QUICK_REPLY",
-            //                     "text": "Unsubcribe from Promos"
-            //                   },
-            //                   {
-            //                     "type":"QUICK_REPLY",
-            //                     "text": "Unsubscribe from All"
-            //                   }
-            //                 ]
-            //               }
-            //             ]
-            //           }
-            //         ,
-            //         headers: {
-            //           "Content-Type": "application/json"
-            //         }
-            //       })
-            //         .then(response => {
-            //           console.log("Message sent successfully:", response.data);
-            //           res.sendStatus(200);
-            //         })
-            //         .catch(error => {
-            //           res.sendStatus(400); 
-            //         });
-            //     }
-            // }
-        }
+    }
 });
 
 app.get("/",(req,res)=>{
