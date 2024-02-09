608795ddffe84984c65c9d518ae8ed68011992af
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2024-02-09 17:21:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.post("/webhook",(req,res)=>{
             axios({
                 method: "POST",
                 url: "https://graph.facebook.com/v18.0/" + phon_no_id + "/messages?access_token=" + token,
-                data: msg_body === "hi" ? template_customn : isBusinessId(process.env.BusinessID)
+                data: msg_body === "hi" ? template_customn : template_customn
                 ,
                 headers: {
                   "Content-Type": "application/json"
